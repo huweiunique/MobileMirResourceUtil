974aa27181a98cdb41c5eb81670cf0c6aa426b23
huwei <weiwei.hu@foxmail.com>
2019-09-05 16:47:34 +0800

    增加注释及测试代码

--- MobileMirResourceUtil/ResourceInfo.cs
+++ MobileMirResourceUtil/ResourceInfo.cs
@@ -10,8 +10,12 @@ using Swifter.Json;
 
 namespace MobileMirResourceUtil
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ResourceInfo
     {
+        public string FileName { get; set; }
         /// <summary>
         /// 文件地址偏移量
         /// </summary>
@@ -24,6 +28,15 @@ namespace MobileMirResourceUtil
 
         public async Task DePackageAsync(string sourceFile)
         {
+            /*
+             * 
+            最后一位为:8的偏移量是:17,对应资源文件:cboeffect.zip,cbohair.zip,cbohum.zip,cboweapon.zip,chrsel.zip,effect.zip,hair.zip,hair2.zip,hum.zip,hum2.zip,humeffect.zip,magic.zip,magic2.zip,magic3.zip,magic4.zip,magic5.zip,magic6.zip,mon-kulou.zip,mon1.zip,mon10.zip,mon11.zip,mon12.zip,mon13.zip,mon14.zip,mon15.zip,mon16.zip,mon17.zip,mon18.zip,mon19.zip,mon2.zip,mon20.zip,mon21.zip,mon22.zip,mon23.zip,mon24.zip,mon28.zip,mon3.zip,mon4.zip,mon5.zip,mon6.zip,mon7.zip,mon8.zip,mon9.zip,npc.zip,objects.zip,objects10.zip,objects13.zip,objects14.zip,objects2.zip,objects3.zip,objects4.zip,objects5.zip,objects6.zip,objects7.zip,objects8.zip,objects9.zip,prguse.zip,prguse2.zip,prguse3.zip,smtiles.zip,tiles.zip,weapon.zip,weapon2.zip
+
+            最后一位为:16的偏移量是:0,对应资源文件:dnitems.zip,hair2_ball.zip,hum3_ball.zip,items.zip,magic10.zip,magic7.zip,magic8.zip,mon34.zip,stateitem.zip,ui1.zip
+
+            最后一位为:0的偏移量是:0,后9位全是0,对应资源文件:mmap.zip,上一层目录的rs.zip
+            */
+            this.FileName = Path.GetFileName(sourceFile);
             ChildFiles = new List<ChildFile>();
             using (var stream = CreateReadStream(sourceFile))
             {
@@ -71,7 +84,7 @@ namespace MobileMirResourceUtil
                     //Console.WriteLine(childFile.ToString());
                 }
             }
-
+            //早期代码通过计算偏移量得到是否有17,后期分析后,发现最后9位的第9位对应的字节,如果是8就是偏移17,16和0都是偏移0,具体规则请看此方法的第一句注释
             AutoCalcFileLengthOffset();
             SaveToFile(sourceFile);
         }
@@ -98,7 +111,7 @@ namespace MobileMirResourceUtil
         private void WritePackageInfo(string dir)
         {
             string json = JsonFormatter.SerializeObject(this);
-            File.WriteAllText(Path.Combine(dir, "descript.txt"), json, Encoding.UTF8);
+            File.WriteAllText(Path.Combine(dir, "description.txt"), json, Encoding.UTF8);
         }
 
         private void AutoCalcFileLengthOffset()
@@ -155,13 +168,31 @@ namespace MobileMirResourceUtil
 
     public class ChildFile
     {
+        private byte[] _lastNineBytes;
         public string FileName { get; set; }
 
 
         /// <summary>
         /// 最后9位参数记录的数据
         /// </summary>
-        public byte[] LastNineBytes { get; set; }
+        public byte[] LastNineBytes
+        {
+            get => _lastNineBytes;
+            set
+            {
+                _lastNineBytes = value;
+                SetLastByteDescription(_lastNineBytes);
+            }
+        }
+
+        private void SetLastByteDescription(byte[] lastNineBytes)
+        {
+            if (lastNineBytes == null || lastNineBytes.Length != 9)
+                return;
+            LastByteDescript =
+                $"坐标X:{BitConverter.ToInt16(lastNineBytes, 0)},坐标Y:{BitConverter.ToInt16(lastNineBytes, 2)},图片宽度:{BitConverter.ToInt16(lastNineBytes, 4)},高度:{BitConverter.ToInt16(lastNineBytes, 6)},最后一位(猜测是分类代码):{lastNineBytes[8]}";
+        }
+        public string LastByteDescript { get; set; }
         /// <summary>
         /// 偏移量
         /// </summary>
@@ -187,7 +218,14 @@ namespace MobileMirResourceUtil
 
         private async Task SaveFileAsync(string dir, byte[] data)
         {
-            using (var stream = File.Create(Path.Combine(dir, this.FileName)))
+            string path = Path.Combine(dir, this.FileName);
+            if (this.FileName.IndexOfAny(new[] { '/', '\\' }) != -1)
+            {
+                string childDir = Path.GetDirectoryName(path);
+                if (!Directory.Exists(childDir))
+                    Directory.CreateDirectory(childDir);
+            }
+            using (var stream = File.Create(path))
             {
                 await stream.WriteAsync(data, 0, data.Length).ConfigureAwait(false);
             }
